a557225c9173f3faec6cb5e83e8dedef146d77d3
Chioreanu Filip-Andrei <chioreanufilip@gmail.com>
2025-11-12 19:07:23 +0200

    getAllEvents fixed

--- src/main/java/com/event_planner/event_planner/controller/LoginRequest.java
+++
@@ -1,7 +0,0 @@
-package com.event_planner.event_planner.controller;
-import lombok.Data;
-@Data
-public class LoginRequest {
-    private String email;
-    private String password;
-}
--- src/main/java/com/event_planner/event_planner/service/JwtService.java
+++ src/main/java/com/event_planner/event_planner/service/JwtService.java
@@ -43,7 +43,7 @@ public class JwtService {
                 .setClaims(extraClaims)
                 .setSubject(userDetails.getUsername()) // Aici se pune email-ul
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24*60*7)) // Setează să expire într-o zi
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24*60*7))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
